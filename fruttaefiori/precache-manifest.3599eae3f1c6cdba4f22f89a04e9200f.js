89adbcc8cffd32e53aec63efcfdfc4d31a67b46a
Andrea Frittella <me.liveset@gmail.com>
2020-08-28 12:21:39 +0200

    Frutta e fiori

---
+++ fruttaefiori/precache-manifest.3599eae3f1c6cdba4f22f89a04e9200f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df74c202fa3926903df1f8c4a2e83e97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abb295c6c5e4451fd572",
+    "url": "/static/css/main.71f6c686.chunk.css"
+  },
+  {
+    "revision": "bd757e710ed6ae0a9590",
+    "url": "/static/js/2.2a099072.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2a099072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb295c6c5e4451fd572",
+    "url": "/static/js/main.043e323d.chunk.js"
+  },
+  {
+    "revision": "5418941eb8fa98550e7b",
+    "url": "/static/js/runtime-main.0df179c4.js"
+  }
+]);
